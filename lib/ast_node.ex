13ea6fb80da1e6ebf17db1974168f9f48a4c207f
Dana Scheider <dana.scheider@gmail.com>
2016-08-11 11:12:42 -0700

    Add get_single\2 function

--- lib/ast_node.ex
+++ lib/ast_node.ex
@@ -1,13 +1,17 @@
 defmodule Gherkin.AstNode do
   defstruct rule_type: :GherkinDocument, sub_items: %{}
 
-  def add(node, rule_type, item) do
-    new_sub_items = if Map.has_key?(node.sub_items, rule_type) do
-      %{node.sub_items | rule_type => Map.get(node.sub_items, rule_type) ++ [item]}
+  def add(ast_node, rule_type, item) do
+    new_sub_items = if Map.has_key?(ast_node.sub_items, rule_type) do
+      %{ast_node.sub_items | rule_type => Map.get(ast_node.sub_items, rule_type) ++ [item]}
     else
-      Map.merge(node.sub_items, %{rule_type => [item]})
+      Map.merge(ast_node.sub_items, %{rule_type => [item]})
     end
 
-    %{node | sub_items: new_sub_items}
+    %{ast_node | sub_items: new_sub_items}
+  end
+
+  def get_single(ast_node, rule_type) do
+    Map.get(ast_node.sub_items, rule_type, []) |> List.first
   end
 end
--- test/ast_node_test.exs
+++ test/ast_node_test.exs
@@ -3,9 +3,15 @@ defmodule GherkinAstNodeTest do
   doctest Gherkin.AstNode
 
   test ".add\\2 adds a sub item to sub-items" do
-    node  = %Gherkin.AstNode{}
-    token = %Gherkin.Token{type: :FeatureLine, matched_keyword: "Feature", matched_text: "Foo", line: %Gherkin.Line{text: "Feature: Foo"}}
+    ast_node  = %Gherkin.AstNode{}
+    token     = %Gherkin.Token{type: :FeatureLine, matched_keyword: "Feature", matched_text: "Foo", line: %Gherkin.Line{text: "Feature: Foo"}}
 
-    assert Gherkin.AstNode.add(node, :FeatureLine, token) == %{node| sub_items: %{FeatureLine: [token]}}
+    assert Gherkin.AstNode.add(ast_node, :FeatureLine, token) == %{ast_node| sub_items: %{FeatureLine: [token]}}
+  end
+
+  test ".get_single\\2 gets the first sub-item with the given type" do
+    ast_node = %Gherkin.AstNode{rule_type: :FeatureLine, sub_items: %{ScenarioLine: [%Gherkin.Token{type: :ScenarioLine, matched_text: "Foo"}, %Gherkin.Token{type: :ScenarioLine, matched_text: "Bar"}]}}
+
+    assert Gherkin.AstNode.get_single(ast_node, :ScenarioLine) == %Gherkin.Token{type: :ScenarioLine, matched_text: "Foo"}
   end
 end
